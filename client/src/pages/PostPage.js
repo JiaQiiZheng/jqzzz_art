b7d0d8fb004386a073c997ce6b9cc41477362983
Jiaqi <dxq7nz@virginia.edu>
2023-03-13 02:01:59 -0400

    last

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -4,13 +4,14 @@ import { formatISO9075 } from "date-fns";
 import { UserContext } from "../UserContext";
 import { Link } from "react-router-dom";
 import Image from "../image";
+const baseUrl = process.env.REACT_APP_API_URL;
 
 export default function PostPage() {
   const [postInfo, setPostInfo] = useState(null);
   const { userInfo } = useContext(UserContext);
   const { id } = useParams();
   useEffect(() => {
-    fetch(`http://localhost:4000/post/${id}`).then((response) => {
+    fetch(`${baseUrl}/post/${id}`).then((response) => {
       response.json().then((postInfo) => {
         setPostInfo(postInfo);
       });
